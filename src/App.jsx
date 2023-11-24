34293dc8f6bff3220e38e08a19554fdec475d5ea
Guillermo San Martín <guille.seba0802@gmail.com>
2023-11-24 18:11:37 -0300

    loading status

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import CategoryPage from './components/CategoryPage';
 import Navbar from './components/Navbar';
 import ThemeContextContainer from './components/ThemeContext';
 import Home from './pages/Home';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { useLabsStore } from './store/labs';
 import Article from './components/Article';
+import { Loading } from './components/Loading';
 
 function App() {
   const { fetchLabs } = useLabsStore(state => state);
@@ -13,16 +14,18 @@ function App() {
     fetchLabs();
   }, []);
   return (
-    <ThemeContextContainer>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/*' element={<Home />} />
-          <Route path='/category/:name' element={<CategoryPage />} />
-          <Route path='/category/:name/article/:id' element={<Article />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeContextContainer>
+    <Suspense fallback={<Loading />}>
+      <ThemeContextContainer>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/*' element={<Home />} />
+            <Route path='/category/:name' element={<CategoryPage />} />
+            <Route path='/category/:name/article/:id' element={<Article />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContextContainer>
+    </Suspense>
   );
 }
 
